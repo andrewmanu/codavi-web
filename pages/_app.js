31ecc7bafd1d94dc4de7b76869dcb90ba75f7ac2
andrewmanu <80987889+andrewmanu@users.noreply.github.com>
2022-11-17 16:47:40 -0300

    update description (#1)
    Co-authored-by: Manu<mcabral@frba.utn.edu.ar>

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export default function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta
           name="description"
-          content="Ponete a tanto sobre la vacunación contra el COVID-19 en la Argentina."
+          content="Averigua como fue la vacunación contra el COVID-19 en la Argentina."
         />
         <meta
           name="keywords"
