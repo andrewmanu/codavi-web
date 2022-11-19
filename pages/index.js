3902babdebf362277656f0631b7d07ab2dc07c48
Manuel Cabral <mcabral@frba.utn.edu.ar>
2022-11-19 20:23:52 -0300

    Revert "Update description"

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({
       <Container>
         <Title>CODAVI</Title>
         <Description>
-          Visualización y estadísticas sobre el COVID-19 en toda la Argentina. Obsotelo por el momento, hasta que se desarrolle una versión más nueva.
+          Visualización y estadísticas sobre el COVID-19 en toda la Argentina.
         </Description>
       </Container>
       <Subcontainer>
